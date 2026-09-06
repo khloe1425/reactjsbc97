e92c26f243005525b20d709f4f807c41f61a84b3
Nguyen <tranchauthanhnguyen@gmail.com>
2026-09-06 20:33:43 +0700

    reactjs code b1

---
+++ src/components/HeaderHome.jsx
@@ -0,0 +1,16 @@
+//? function component : là một function trả về 1 đoạn jsx (các thành phần UI) trong reactjs
+
+// rafce: tạo nhanh reactjs function component
+
+
+ const HeaderHome = () => {
+  return (
+    <div>
+        <img src="/hero.png" alt="Logo" />
+          <img src="https://salt.tikicdn.com/cache/280x280/ts/product/ac/a7/e1/a6f56feaea63d36f4e28110edc7fac6c.png.webp" alt="Hình sản phẩm" />
+    </div>
+  )
+}
+
+//chỉ được có 1 export default trong 1 file jsx
+export default HeaderHome
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
 import { createRoot } from 'react-dom/client'
+import HeaderHome from './components/HeaderHome'
 
 //? jsx : chứa các code tạo nên các đối tương component(các thành phần UI)  trong reactjs
 //? complier : chuyển đổi các code jsx sang các code js & html thuần 
 //? lệnh chạy npm run dev : chạy ứng dụng reactjs (biên dịch code , chạy localhost)
 //? lệnh build npm run build : build code reactjs sang code js & html thuần để deploy lên server => đóng gói code (bundle) reactjs thành các file js & html thuần để deploy lên server (một file js, một file html, một file css, ...)
 createRoot(document.getElementById('root')).render(
- <h1>Render reactjs</h1>
+  <>
+    {/* <h1>Render reactjs</h1>
+    <HeaderHome />
+   */}
+   
+  </>
+
 )
+
+
+//render(đối số): truyền 1 đối tương 
+// bọc trong 1 thẻ cha (div), thẻ fragment (<> </>) 
+// tạo hàm (tham số): parameter 
+// gọi hàm (giá trị truyền vào là đối số) argument
